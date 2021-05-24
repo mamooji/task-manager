1dc85adf1fe665060965e56c55bc10b0eb18b5cf
Muhammad Mamooji <mamoojim@hotmail.com>
2021-05-24 14:48:54 -0400

    added middle ware and authentication

--- src/index.js
+++ src/index.js
@@ -1,20 +1,46 @@
 //import statements
 import express from "express";
 import "./db/mongoose.js";
+import jwt from "jsonwebtoken";
 
 //importing models
-
 import { userRouter } from "./routers/user.js";
 import { taskRouter } from "./routers/task.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   console.log(req.method, req.path);
+//   if (req.method === "GET") {
+//     res.send("GET requests are diabled");
+//   } else {
+//     next();
+//   }
+// });
+
+app.use((req, res, next) => {
+  res.status(503).send("site is in maintenance currently");
+});
+
+//using our routers we imported
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-//setver
+//server
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
+
+const myFunction = async () => {
+  const token = jwt.sign({ _id: "abc123" }, "thisismynewcourse", {
+    expiresIn: "7 days",
+  });
+  console.log(token);
+
+  const data = jwt.verify(token, "thisismynewcourse");
+  console.log(data);
+};
+
+myFunction();
--- src/routers/user.js
+++ src/routers/user.js
@@ -6,15 +6,31 @@ router.get("/test", (req, res) => {
   res.send("from a new file");
 });
 
-//saving a user
+//saving a user/signup
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+//login a user
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send();
   }
 });
 
@@ -55,10 +71,12 @@ router.patch("/users/:id", async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await User.findById(req.params.id);
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+
+    await user.save();
+
     if (!user) {
       return res.status(404).send();
     }
